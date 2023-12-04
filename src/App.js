023085ee98c0e5c8fb00a916984aec43bc1c272f
lisandro cordoba <lisandrocba_7@hotmail.com>
2023-12-04 16:53:24 -0300

    Trabajo finalizado cargad de imagenes con muestra de base de datos tanto front como back

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
   const [archivos, setArchivos] = useState({
     name : '',
     archivoFile: null
   })
-  return (
-    <div className="bg-slate-800 text-white h-screen w-screen flex flex-col justify-center items-center">
-      <h1 className="text-2xl">Formulario para cargar archivos en AWS s3</h1>
-      <form className="flex flex-col justify-center items-center bg-slate-900 rounded-3xl p-10 my-3 shadow-lg shadow-slate-700 w-1/3">
-        <input
-          className="rounded-md bg-slate-700 px-3 py-1 mb-5 text-center"
-          placeholder="Nombre del archivo"
-          type="text"
-          onChange={e=> setArchivos({...archivos, name : e.target.value})}
-        />
+  const [imagesS3, setImagesS3] = useState()
 
+  useEffect(()=>{
+    const data =async()=>{
+      await axios.get('http://localhost:4000/allimage').then(res=> setImagesS3(res))
+      
+    }
+
+    data()
+  },[])
+  
+
+  console.log(archivos.archivoFile)
+
+  const handleSubmint = async(e) => {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append("photo", archivos.archivoFile);
+  
+    const res = await axios.post('http://localhost:4000/upload', formData, {
+      headers: {
+        "content-type": "multipart/form-data"
+      }
+    });
+  
+    console.log(res);
+  }
+
+ 
+  return (
+    <div className="bg-slate-800 text-white h-full flex flex-col justify-center items-center">
+      <h1 className="text-2xl">Formulario para cargar archivos en AWS s3</h1>
+      <form className="flex flex-col justify-center items-center bg-slate-900 rounded-3xl p-10 my-3 shadow-lg shadow-slate-700 w-1/3" onSubmit={e=>handleSubmint(e)}>
         <div className="flex items-center justify-center w-full">
           <label
             htmlFor="dropzone-file"
@@ -48,11 +71,25 @@ function App() {
             <input id="dropzone-file" type="file" className="hidden" onChange={e=> setArchivos({...archivos, archivoFile: e.target.files[0]})} />
           </label>
         </div>
-
         <button className="bg-slate-800 hover:bg-slate-700 px-5 py-3 rounded-lg mt-4">
           Cargar datos
         </button>
       </form>
+      <h3 className="text-3xl mt-10">Imagenes guardadas en la base de datos AWS s3:</h3>
+      <div className="flex flex-row flex-wrap justify-center items-center bg-slate-900 rounded-3xl p-10 my-3 shadow-lg shadow-slate-700 w-3/4">
+
+      {
+        
+       imagesS3 && imagesS3.data.resp.map(img=>{
+         return(
+           
+           <img className='w-1/5 px-3 py-2' src={img.url} alt={img.key}/>
+           
+           )
+          })
+        }
+        </div>
+      
     </div>
   );
 }
